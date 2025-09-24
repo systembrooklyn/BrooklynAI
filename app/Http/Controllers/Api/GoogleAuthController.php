5c6a3ca153565a8a98c3bb23e86f8e156f8ec587
system_brooklyn <system.bbs.2024@gmail.com>
2025-09-24 13:47:27 +0300

    refactor GoogleAuthController to remove stateless method calls in redirect and callback

--- app/Http/Controllers/Api/GoogleAuthController.php
+++ app/Http/Controllers/Api/GoogleAuthController.php
@@ -17,7 +17,8 @@ class GoogleAuthController extends Controller
      */
     public function redirect()
     {
-        return Socialite::driver('google')->stateless()->redirect();
+        // return Socialite::driver('google')->stateless()->redirect();
+        return Socialite::driver('google')->redirect();
     }
 
 
@@ -28,7 +29,8 @@ class GoogleAuthController extends Controller
     {
         try {
 
-            $googleUser = Socialite::driver('google')->stateless()->user();
+            // $googleUser = Socialite::driver('google')->stateless()->user();
+            $googleUser = Socialite::driver('google')->user(); 
 
 
             $user = User::withTrashed()->where('google_id', $googleUser->id)->first();
